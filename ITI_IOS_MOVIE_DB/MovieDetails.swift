8ac4fbfaef133b15cb47839388b98275ad522bd2
alyserry <alyserry@gmail.com>
2020-03-13 20:33:18 +0200

    moviedetails1

---
+++ ITI_IOS_MOVIE_DB/MovieDetails.swift
@@ -0,0 +1,42 @@
+//
+//  MovieDetails.swift
+//  ITI_IOS_MOVIE_DB
+//
+//  Created by Aly Ayoub on 3/13/20.
+//  Copyright © 2020 Muhammad El-Arabi. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetails: UIViewController {
+
+    @IBOutlet weak var movieTitle: UILabel!
+    @IBOutlet weak var releaseDate: UILabel!
+    @IBOutlet weak var overview: UITextView!
+    
+    @IBOutlet weak var poster: UIImageView!
+    
+    @IBAction func btnReviews(_ sender: UIButton) {
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+//        var movie = Movie(parameter: <#JSON#>)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
